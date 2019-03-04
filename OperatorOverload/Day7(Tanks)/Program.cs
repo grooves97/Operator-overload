8193aa12141bd59ca855c12d3a1273fcc25cdde4
Aseka <grooves97@gmail.com>
2019-03-04 07:55:18 +0600

    Ended

---
+++ OperatorOverload/Day7(Tanks)/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7_Tanks_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int tankAmount = 5, battles = 5;
+            Tank[] t34 = new Tank[tankAmount];
+            Tank[] pantera = new Tank[tankAmount];
+
+            for (int i = 0; i < tankAmount; i++)
+            {
+                t34[i] = new Tank();
+                pantera[i] = new Tank();
+            }
+
+            bool[] winner = new bool[battles];
+
+            try
+            {
+                for (int i = 0; i < tankAmount; i++)
+                {
+                    if (t34[i]._ammunition == 0 || t34[i]._armorLevel == 0 || t34[i]._maneuverability == 0 ||
+                        pantera[i]._ammunition == 0 || pantera[i]._armorLevel == 0 || pantera[i]._maneuverability == 0) { throw new Exception(); break; }
+                    else
+                        winner[i] = t34[i] * pantera[i];
+                }
+            }
+            catch
+            {
+                Console.WriteLine("The battles was stopped, since one or more participant are disqualification");
+            }
+
+            for (int i = 0; i < battles; i++)
+            {
+                if (winner[i])
+                    Console.WriteLine("Pantera has won!!!!!!!");
+                else
+                    Console.WriteLine("T-34 has won!!!!!!!!!!");
+            }
+            Console.WriteLine(t34[1].GetTankStat());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperatorOverload/MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        public int _ammunition { get; set; }
+        public int _armorLevel { get; set; }
+        public int _maneuverability { get; set; }
+
+        Random rand = new Random(((int)DateTime.Now.Ticks));
+
+        public Tank()
+        {
+
+            _ammunition = rand(0,100);
+            _armorLevel = ;
+            _maneuverability = ;
+        }
+
+        public static bool operator *(Tank obj1,Tank obj2)
+        {
+            int winnerObj1 = 0;
+            int requiredQuantity = 2;
+            if (obj1._ammunition > obj2._ammunition) ++winnerObj1;
+            if (obj1._armorLevel > obj2._armorLevel) ++winnerObj1;
+            if (obj1._maneuverability > obj2._maneuverability) ++winnerObj1;
+
+            return winnerObj1 >= requiredQuantity;
+        }
+            
+        public string GetTankStat()
+        {
+            string stats = "Ammunition = " + _ammunition.ToString() + " Armor level = " + _armorLevel.ToString() + " Maneuverability = " + _maneuverability.ToString();
+            return stats;
+        }
+
+    }
+}
